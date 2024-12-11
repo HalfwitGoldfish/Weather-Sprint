f22b53207d0d5147ef8661ba1b6f7824bd057589
HalfwitGoldfish <Chas2687@gmail.com>
2024-12-10 16:04:03 -0800

    current progress

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,25 @@
 import {APIKEY} from "./apikey.js";
 
 navigator.geolocation.getCurrentPosition(function(position){
+    console.log(position);
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-    async function apiCall(){
 
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`);
-        const data = await response.json();
-        console.log(data);
-
-        const fiveDay = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
-        const fiveDayData = await fiveDay.json();
-        console.log(fiveDayData);
-        
-        return data, fiveDayData;
-        
-    }
-    apiCall();
+    currentWeatherAPI(lat, lon);
 });
+
+async function currentWeatherAPI(lat, lon){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`);
+    const data = await response.json();
+    console.log(data);
+}
+
+async function fiveDayAPI(lat, lon){
+    const fiveDay = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`);
+    const fiveDayData = await fiveDay.json();
+    console.log(fiveDayData);
+    
+    return data, fiveDayData;
+}
+
+// find api that lists states
